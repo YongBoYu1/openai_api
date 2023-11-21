6460a060248973018938ed16e6c814e393faff0d
yoyongbo <yoyu@upei.ca>
2023-11-21 18:19:13 -0500

    added mofidication

--- app.py
+++ app.py
@@ -4,31 +4,85 @@ import whisper
 from moviepy.editor import VideoFileClip
 import openai
 import json
+import requests
+import io
+import codecs 
 
+
+# Reading api keys from file
+api_key = open('api_key.txt', 'r').read().strip('\n')
+api_key
+openai.api_key = api_key
 conversation_history = []
+
+
 def video_identity(video):
     return video
 
-#UI todo: audio to text.
-demo = gr.Interface(video_identity, 
-                    gr.Video(), 
-                    "playable_video"
-                    )
+
+def text_to_audio(text_input, file_name):
+    """This function uses the tts-1 model from OPENAI to convert input str to audio and 
+    save under test,
+
+    Args:
+        text_input (str): _description_
+    """
+    response = requests.post(
+    'https://api.openai.com/v1/audio/speech',
+    headers={
+        "Authorization":f"Bearer {api_key}"
+    },
+    json = {
+        'model':'tts-1',
+        'input': text_input, #todo: change this to a variable
+        'voice':'nova', #check https://platform.openai.com/docs/guides/text-to-speech for voice samples
+    },
+    )
+    if response.status_code != 200:
+        print("Request failed !!")
+        print(response.status_code)
+
+    audio_byte_io = io.BytesIO()
+    for chunk in response.iter_content(chunk_size=1024*1024):
+        audio_byte_io.write(chunk)
+        
+    audio_byte_io.seek(0)
+
+    with open(f'{file_name}.wav', 'wb') as tmpFile:
+        for chunk in response.iter_content(chunk_size=1024*1024):
+            tmpFile.write(chunk)
+        audio_file_name = tmpFile.name
+    return audio_file_name, audio_byte_io
+
 
 
 #Extract the audio from video
-def convert_video_to_audio_moviepy(video_file, suffix="wav"):
-    """Converts video to audio using MoviePy library
-    that uses `ffmpeg` under the hood"""
+def extract_video_audio_moviepy(video_file, suffix="wav"):
+    """Extract the audio from video ans save to .wav file 
+
+    Args:
+        video_file (str): video file path
+        suffix (str, optional): _description_. Defaults to "wav".
+    """
     filename, ext = os.path.splitext(video_file)
     clip = VideoFileClip(video_file)
     clip.audio.write_audiofile(f"{filename}.{suffix}")
 
+
 # Use Whisper, audio to text
 def whisper_to_text(audio_file_path):
+    """Use the Whisper model from 
+
+    Args:
+        audio_file_path (str): Convert audio to text
+
+    Returns:
+        _type_: _description_
+    """
     model = whisper.load_model('small')
     result = model.transcribe(audio_file_path)
     return result['text']
+
 
 # Modify text from chatgpt
 def build_chat(input, prompt, role='user'):
@@ -59,6 +113,34 @@ def build_chat(input, prompt, role='user'):
     conversation_history.append({"role": 'assistant', "content": reply_str})
     return reply_str
 
+
+def main():
+
+    # prompt msg for the chat GPT
+    prompt_msg = 'modify my input text in the same language, but without changing the meaning and keep the similar length. If you understand, say OK.'
+    prompt = [{"role": "user", "content": prompt_msg},
+            {"role": "assistant", "content": f"OK"}]
+    
+    # todo: get a sample video file, replace line 124-127
+    # todo: UI
+    video_file = ""
+    
+    with codecs.open('movie_descip.json', 'r', 'utf-8') as file:
+        data = json.load(file)
+    text = data['实验']
+
+    
+    file_name, _ = text_to_audio(text, 'orgin_text')
+    text_modified = build_chat(text, prompt, role='user')
+    # todo: save the text and modified text. Check the different
+    temp_dic = {}
+    temp_dic['origin'] = text
+    temp_dic['modify'] = text_modified
+    with codecs.open('temp_dic.json', 'w', encoding='utf-8') as f:
+        json.dump(temp_dic, f, ensure_ascii=False)
+    file_name, io_stream = text_to_audio(text_modified, 'text_modified')
+
+
     
 if __name__ == "__main__":
     # steps 1. upload the video to gradio ui. 
@@ -68,5 +150,7 @@ if __name__ == "__main__":
     # 5. Text from 4, to audio
     # 6. Extract video
     # 7. stitch the 6, 5 a
-    # 8. ouput
-    demo.launch()
+    # 8. ouput the final video
+    
+    main()
+    print('DOne')
